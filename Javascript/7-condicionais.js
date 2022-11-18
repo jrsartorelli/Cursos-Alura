b4b0f4fe53db08a7bf841d447d6c5f6fe416d5ba
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-11-18 14:29:01 -0300

    progresso

--- Javascript/7-condicionais.js
+++ Javascript/7-condicionais.js
@@ -1,6 +1,6 @@
 console.log(`Trabalhando com condicionais e operadores lógicos`);
 
-const listaDeDestinos = new Array (
+const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
@@ -8,19 +8,33 @@ const listaDeDestinos = new Array (
 
 listaDeDestinos.push(`Curitiba`);
 
-const idadeComprador = 16;
-const estaAcompanhada  = false;
+const idadeComprador = 18;
+const estaAcompanhada = true;
+const temPassagemComprada = true;
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1); // removendo item
-} else if (estaAcompanhada == true) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1); //removendo item
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); // removendo item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) { 
+    console.log("Boa Viagem!");
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem");
+}else{
+    console.log("Você não pode embarcar");
+} 
 
 console.log(listaDeDestinos);
---
+++ Javascript/8-loops.js
@@ -0,0 +1,33 @@
+console.log(`Trabalhando com loops`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Salvador";
+
+const podeComprar =  idadeComprador >= 18 || estaAcompanhada == true;
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem!!");
+    listaDeDestinos.splice(2,1); 
+    temPassagemComprada = true;
+} else {
+    console.log("Não é maior de Idade e não posso vender");
+    temPassagemComprada = false;
+}
+
+let contador = 0
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe") 
+    }else{ 
+        console.log("Destino não existe");
+    }
+    contador += 1;
+} 
